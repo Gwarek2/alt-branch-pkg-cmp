d057d62ba5f39c0f12219c037ec534e22d79f52e
Gwarek2 <fedorsah@gmail.com>
2022-10-30 20:00:48 +0300

    Release version 0.1

---
+++ src/pkgset.py
@@ -0,0 +1,81 @@
+import requests
+from collections import defaultdict
+from dataclasses import dataclass, asdict
+from packaging import version
+from typing import Optional
+from urllib.parse import urljoin
+
+
+@dataclass
+class Package:
+    name: str
+    epoch: str
+    version: str
+    release: str
+    arch: str
+    disttag: str
+    buildtime: str
+    source: str
+
+
+class PackageSet:
+    def __init__(self, branch: str, packages: Optional[dict] = None) -> None:
+        self.branch = branch
+        self.packages = packages if packages else defaultdict(dict)
+
+    def add(self, pkg: Package) -> None:
+        if not isinstance(pkg, Package):
+            raise TypeError(f"{pkg} is not instance of {Package}")
+        self.packages[pkg.arch][pkg.name] = pkg
+
+    def diff(self, pkgset: "PackageSet") -> dict:
+        self._validate_pkgset(pkgset)
+        result = defaultdict(list)
+        for arch, pkgs1 in self.packages.items():
+            for name, pkg in pkgs1.items():
+                if not pkgset.packages.get(arch, {}).get(name):
+                    result[arch].append(asdict(pkg))
+        return dict(result)
+
+    def newer_than(self, pkgset: "PackageSet") -> dict:
+        self._validate_pkgset(pkgset)
+        result = defaultdict(list)
+        for arch, pkgs1 in self.packages.items():
+            for name, pkg1 in pkgs1.items():
+                pkg2 = pkgset.packages.get(arch, {}).get(name)
+                if pkg2 and version.parse(pkg1.version) > version.parse(pkg2.version):
+                    result[arch].append(asdict(pkg1))
+        return dict(result)
+
+    def _validate_pkgset(self, pkgset) -> None:
+        if not isinstance(pkgset, PackageSet):
+            raise TypeError(f"{pkgset} is not an instance of {PackageSet}")
+
+    def __repr__(self):
+        output = {}
+        for arch, pkgs in self.packages.items():
+            output[arch] = [pkg for pkg in pkgs.values()]
+        return str(output)
+
+
+api_url = "https://rdb.altlinux.org"
+
+
+def get_branch_bin_pkgs(branch: str) -> dict:
+    url_path = urljoin(api_url, f"api/export/branch_binary_packages/{branch}")
+    response = requests.get(url_path)
+    if response.status_code // 100 == 2:
+        return response.json()
+    print(
+        f"Failed to retrive branch '{branch}' package lists. "
+        f"Http error: {response.status_code}"
+    )
+    return {}
+
+
+def create_pkgsets(pkgs: list[dict], branch: str) -> PackageSet:
+    result = PackageSet(branch)
+    for pkg_data in pkgs:
+        pkg = Package(**pkg_data)
+        result.add(pkg)
+    return result
---
+++ src/tests/test_pkgset.py
@@ -0,0 +1,57 @@
+import json
+import unittest
+from pathlib import Path
+from pkgset import Package, PackageSet, create_pkgsets
+
+
+data_path = Path(__file__).parent / "data"
+
+
+def read_json_file(file_path):
+    return json.loads(file_path.read_text())
+
+
+class TestPackageSet(unittest.TestCase):
+    def setUp(self):
+        pkgs1 = read_json_file(data_path / "dataset1.json").get("packages")
+        pkgs2 = read_json_file(data_path / "dataset2.json").get("packages")
+        self.pkgset1 = create_pkgsets(pkgs1, "sisyphus")
+        self.pkgset2 = create_pkgsets(pkgs2, "p10")
+
+    def test_adding_invalid_packages(self):
+        with self.assertRaises(TypeError):
+            self.pkgset1.add(None)
+        with self.assertRaises(TypeError):
+            self.pkgset2.add([1, 2, 3, 4])
+
+    def test_adding_packages(self):
+        package = Package(
+            name="gpg",
+            epoch="0",
+            version="2.1.7",
+            release="atl1",
+            arch="x86_64",
+            disttag="sisyphus",
+            buildtime="123432324",
+            source="Cardinal",
+        )
+        self.pkgset1.add(package)
+        self.pkgset2.add(package)
+        self.assertEqual(package, self.pkgset1.packages["x86_64"]["gpg"])
+        self.assertEqual(package, self.pkgset2.packages["x86_64"]["gpg"])
+
+    def test_pkgsets_diff(self):
+        diff1 = read_json_file(data_path / "diff1.json")
+        diff2 = read_json_file(data_path / "diff2.json")
+        self.assertEqual(self.pkgset1.diff(self.pkgset2), diff1)
+        self.assertEqual(self.pkgset2.diff(self.pkgset1), diff2)
+
+    def test_pksets_newer_pkgs(self):
+        newer1 = read_json_file(data_path / "newer1.json")
+        newer2 = read_json_file(data_path / "newer2.json")
+        self.assertEqual(self.pkgset1.newer_than(self.pkgset2), newer1)
+        self.assertEqual(self.pkgset2.newer_than(self.pkgset1), newer2)
+
+
+if __name__ == "__main__":
+    unittest.main()
